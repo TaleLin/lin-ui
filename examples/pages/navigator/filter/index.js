802bc18982994340e427ff5cf43b47905fd08eba
nana <1774732369@qq.com>
2019-02-15 19:07:34 +0800

    feat:新增filter-string功能

---
+++ examples/pages/navigator/filter/index.js
@@ -0,0 +1,68 @@
+// pages/navigator/filter/index.js
+const re = /see (chapter \d+(\.\d)*)/i
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    str: 'For more information, see Chapter 3.4.5.1',
+    re: re
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
