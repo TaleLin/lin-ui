84e852a3999cc3ec864119e3dcd231aa0759662d
zcq <1409051283@qq.com>
2019-02-18 20:38:32 +0800

    feat:新增price组件

---
+++ examples/pages/navigator/shopping/shop-navi.js
@@ -0,0 +1,10 @@
+const shopNaviConfigs = [
+  {
+    icon: "/images/component/action-sheet.png",
+    title: "Price",
+    desc: "价格",
+    componentsPath: "/pages/shopping/pages/price/index"
+  },
+]
+
+export default shopNaviConfigs;
