bc70835421eb13bc94f629c2f0ccf8e17e5d8fb7
nana <1774732369@qq.com>
2019-02-20 15:08:48 +0800

    feat:<过滤器>string、array、classnames

--- examples/pages/navigator/filter/index.js
+++ examples/pages/navigator/filter/index.js
@@ -1,68 +1,73 @@
 // pages/navigator/filter/index.js
-const re = /see (chapter \d+(\.\d)*)/i
+const re = 'see (chapter \d+(\.\d)*)'
 Page({
 
   /**
    * Page initial data
    */
   data: {
-    str: 'For more information, see Chapter 3.4.5.1',
-    re: re
+    str: '      For more information, see Chapter 3.4.5.1',
+    arr: [{ a: 1 }, { a: 2 }, { a: 3 }, { a: 4 }],
+    params: { red: true, bg: true },
+    scrollable:true,
+    placement:'left',
+    aminmated:false,
+    foo:'foo-bar'
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+
   },
 
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
