dc747c7b7d3f0ad26183c263d241cc1ce64d02f4
zcq <1409051283@qq.com>
2019-02-28 22:04:43 +0800

    feat:更新markdown代码样式

--- examples/pages/navigator/shopping/shop-navi.js
+++ examples/pages/navigator/shopping/shop-navi.js
@@ -5,6 +5,12 @@ const shopNaviConfigs = [
     desc: "价格",
     componentsPath: "/pages/shopping/pages/price/index"
   },
+  {
+    icon: "/images/component/action-sheet.png",
+    title: "CountSelector",
+    desc: "数量选择器",
+    componentsPath: "/pages/shopping/pages/count-selector/index"
+  },
 ]
 
 export default shopNaviConfigs;
---
+++ examples/pages/shopping/pages/count-selector/count-nav.js
@@ -0,0 +1,30 @@
+const priceNaviConfigs = [{
+  title: "基础用法（max=10,min=1,count=1）",
+  config: [{
+    count: 1,
+    min: 1,
+    max: 10,
+    step: 1,
+    disabled: false
+  }]
+}, {
+  title: "设置步长（step=2）",
+  config: [{
+    count: 1,
+    min: 1,
+    max: 10,
+    step: 2,
+    disabled: false
+  }]
+}, {
+  title: "禁用状态",
+  config: [{
+    count: 1,
+    min: 1,
+    max: 10000,
+    step: 1,
+    disabled: true
+  }]
+}]
+
+export default priceNaviConfigs;
