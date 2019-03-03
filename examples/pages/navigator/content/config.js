3a620492c63e796c66e69aa96f3eeddb46c50e0d
zcq <1409051283@qq.com>
2019-03-03 17:52:19 +0800

    feat:更新demo目录结构

---
+++ examples/pages/navigator/content/config.js
@@ -0,0 +1,19 @@
+import basic from './config/basic-navi.js'
+import response from './config/response-navi.js'
+import layout from './config/layout-navi.js'
+import nav from './config/nav-navi.js'
+import view from './config/view-navi.js'
+import shopping from './config/shop-navi.js'
+import form from './config/form-navi.js'
+
+const config = {
+  basic,
+  response,
+  layout,
+  view,
+  nav,
+  form,
+  shopping
+}
+
+export default config
---
+++ examples/pages/navigator/content/config/response-navi.js
@@ -0,0 +1,34 @@
+const responseNaviConfigs = [
+  {
+    icon: "/images/component/action-sheet.png",
+    title: "ActionSheet",
+    desc: "操作菜单",
+    componentsPath: "/pages/components/response/pages/action-sheet/index"
+  },
+  {
+    icon: "/images/component/message.png",
+    title: "Message",
+    desc:"消息提示",
+    componentsPath: "/pages/components/response/pages/message/index"
+  },
+  {
+    icon: "/images/component/toast.png",
+    title: "Toast",
+    desc: "轻提示",
+    componentsPath: "/pages/components/response/pages/toast/index"
+  },
+  {
+    icon: "/images/component/dialog.png",
+    title: "Dialog",
+    desc: "模态框",
+    componentsPath: "/pages/components/response/pages/dialog/index"
+  },
+  {
+    icon: "/images/component/slide-view.png",
+    title: "SlideView",
+    desc: "滑动菜单",
+    componentsPath: "/pages/components/response/pages/slide-view/index"
+  },
+]
+
+export default responseNaviConfigs;
