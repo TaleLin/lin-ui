8199e51c2ff0fb43322b53fa48d99f3b5995ea30
zcq <1409051283@qq.com>
2019-03-03 17:52:19 +0800

    feat:更新demo目录结构

---
+++ examples/pages/navigator/content/config/basic-navi.js
@@ -0,0 +1,16 @@
+const basicNaviConfigs = [
+  {
+    icon: "/images/component/button.png",
+    title: "Button",
+    desc: "按钮",
+    componentsPath: "/pages/components/basic/pages/button/index"
+  },
+  {
+    icon: "/images/component/icon.png",
+    title: "Icon",
+    desc: "图标",
+    componentsPath: "/pages/components/basic/pages/icon/index"
+  },
+]
+
+export default basicNaviConfigs;
