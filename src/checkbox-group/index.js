7920d2a94165e20f38d2a4d21eb69d33d49f990b
宁缺 <ypchng@qq.com>
2019-04-13 15:13:20 +0800

    feat: checkbox组件的linchange事件可以返回当前所有选中项的内容 (#186)

--- src/checkbox-group/index.js
+++ src/checkbox-group/index.js
@@ -28,24 +28,42 @@ Component({
       value: 'column', //column row
     },
   },
+  data: {
+    value: [],
+    list: [],
+  },
   attached() {
-    this.initRules();
+    this.initRules()
   },
 
   methods: {
     // checkbox change
     onChangeHandle(val = this.data.current) {
+      // let items = this.getRelationNodes('../checkbox/index');
+      // const len = items.length;
+      // if (len) {
+      //   items.forEach(item => {
+      //     let type = val.indexOf(item.data.value) !== -1
+      //     item.onChangeHandle(type);
+      //   });
+      // }
     },
     onEmitEventHandle(current) {
-      const index = this.data.current.indexOf(current.value);
+          
+      const index = this.data.current.indexOf(current.value) 
       index === -1 ? this.data.current.push(current.value) : this.data.current.splice(index, 1);
+      index === -1 ? this.data.list.push(current) : this.data.list.splice(index, 1);
       this.setData({
         current: this.data.current,
-        value: this.data.current
-      },()=>{
-        this.validatorData({value:this.data.value});
-      });  
-      this.triggerEvent('linchange', current);
+        value: this.data.list
+      },() => {
+        this.validatorData({value:this.data.value})
+      })
+      
+      const all = JSON.parse(JSON.stringify(this.data.list))
+      current.all = all
+
+      this.triggerEvent('linchange', current)
     }
   }
 });
