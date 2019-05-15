346367a7a993b0a557cca1cd87d1343206ece6f7
张寸起 <zhangcunqi@zhangcunqideMacBook-Pro.local>
2019-05-15 15:24:22 +0800

    feat:新增steps组件

---
+++ src/step/index.js
@@ -0,0 +1,44 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  relations: {
+    '../steps/index': {
+      type: 'parent'
+    },
+  },
+  properties: {
+    icon: String,
+    status: String,
+    title: String,
+    describe: String,
+    iconSize: {
+      type: Number,
+      value: 24
+    },
+    iconColor: {
+      type: String,
+      value: '#fff'
+    },
+    custom: Boolean
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 与父组件通信并绑定相关配置数据
+    updateDataChange(options) {
+      this.setData({
+        ...options
+      })
+    }
+  }
+})
