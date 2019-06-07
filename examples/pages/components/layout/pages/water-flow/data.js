508d2b6374cc3f6ac07cad74c4ee3116c1575bef
张寸起 <zhangcunqi@zhangcunqideMacBook-Pro.local>
2019-06-07 15:58:15 +0800

    feat:新增water-flow组件

---
+++ examples/pages/components/layout/pages/water-flow/data.js
@@ -0,0 +1,47 @@
+const product = [{
+  image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559893694567&di=decd597a4206cdc46ff1c58f93d54a4e&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F49453126d099fdb7ae6489e01449281c004af3761e10e-tvDbXH_fw658',
+  title: '显瘦中长款系带风衣',
+  describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+  count: '888',
+  delCount: '666'
+},
+{
+  image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559893694566&di=9a26e26b870f520a931c93a817879427&imgtype=0&src=http%3A%2F%2Fpic.rmb.bdstatic.com%2F11a8881842e35c7ff188fbf13f220d5f.jpeg',
+  title: '酵素洗纯棉四件套',
+  describe: '100%新疆精梳棉，柔软、厚实、光泽度好，颜色高级，2色可选',
+  count: '459',
+  delCount: '359'
+},
+{
+  image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559893993143&di=69804e9c197aeef57f69965980d804be&imgtype=0&src=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fblog%2F201311%2F21%2F20131121101949_8iPmK.jpeg',
+  title: '环保棉做的男女款衬衫',
+  describe: '环保的高级长绒精梳棉，亲肤透气，安全无刺激，版型时髦有型，多款多色可选,环保的高级长绒精梳棉，亲肤透气，安全无刺激',
+  count: '888',
+  delCount: '666'
+},
+{
+  image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559893795164&di=bbaa2db719d648f652058efb157735eb&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F2cf875136ae0f6f9babe8fffff24730c00e27c6850d50-8AgKsJ_fw658',
+  title: '自带线的快充移动电源',
+  describe: '自带两条快充线，支持大部分快充设备充电，还能充笔记本电脑，有微电流模式',
+  count: '888',
+  delCount: '666'
+},
+  {
+    image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559895182707&di=afd7d89ebcb21e0dd2f0517336ce8d25&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2FZIrNMh3jc-oib-qFmdyYUQ%3D%3D%2F5717519838063213749.jpg',
+    title: '显瘦中长款系带风衣',
+    describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。不易皱好打理。',
+    count: '888',
+    delCount: '666'
+  },
+  {
+    image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559895137091&di=c7db1a52ecbe3886bf79b39e3da162a4&imgtype=0&src=http%3A%2F%2Fphotocdn.sohu.com%2F20160204%2FImg436843528.jpg',
+    title: '酵素洗纯棉四件套',
+    describe: '100%新疆精梳棉，柔软、厚实、光泽度好，颜色高级，2色可选,100%新疆精梳棉，柔软、厚实、光泽度好，颜色高级，2色可选',
+    count: '459',
+    delCount: '359'
+  }
+]
+
+export {
+  product
+}
