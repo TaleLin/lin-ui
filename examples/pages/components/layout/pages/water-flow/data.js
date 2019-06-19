9ed1be6eec02bdd2a4e3c57f1a002460bab24d1a
aQingYun <37834281+aQingYun@users.noreply.github.com>
2019-06-19 10:14:44 +0800

    feat:1.新增water-flow组件，示例demo新增贡献人员信息 (#306)
    * feat:1.新增water-flow组件2.示例demo新增贡献人员信息
    
    * fix修改water-flow组件传入类型错误提示信息

--- examples/pages/components/layout/pages/water-flow/data.js
+++ examples/pages/components/layout/pages/water-flow/data.js
@@ -8,7 +8,7 @@ const product = [{
 {
   image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559893694566&di=9a26e26b870f520a931c93a817879427&imgtype=0&src=http%3A%2F%2Fpic.rmb.bdstatic.com%2F11a8881842e35c7ff188fbf13f220d5f.jpeg',
   title: '酵素洗纯棉四件套',
-  describe: '100%新疆精梳棉，柔软、厚实、光泽度好，颜色高级，2色可选',
+  describe: '100%新疆精梳棉，柔软、厚实、光泽度好，颜色高级，2色可选颜色高级，2色可选,高级，2色可选',
   count: '459',
   delCount: '359'
 },
@@ -22,7 +22,7 @@ const product = [{
 {
   image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559893795164&di=bbaa2db719d648f652058efb157735eb&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F2cf875136ae0f6f9babe8fffff24730c00e27c6850d50-8AgKsJ_fw658',
   title: '自带线的快充移动电源',
-  describe: '自带两条快充线，支持大部分快充设备充电，还能充笔记本电脑，有微电流模式',
+  describe: '自带两条快充线，支持大部分快充设备充电，还能充笔记本电脑，有微电流模式,电脑，有微电流模式',
   count: '888',
   delCount: '666'
 },
