e19ca4288188cf9938706a1cc52532d5cfd49a61
wangna <wangna08@meicai.com>
2019-07-17 10:43:08 +0800

    fix:tabs动态更新属性

--- src/tabpanel/index.js
+++ src/tabpanel/index.js
@@ -1,12 +1,24 @@
 // components/tab/index.js
 Component({
-  externalClasses: ['v-tab-active', 'tab-default'],
+  externalClasses: ["v-tab-active", "tab-default"],
   relations: {
-    '../tabs/index': {
-      type: 'parent'
+    "../tabs/index": {
+      type: "parent",
+      linked: function(target) {
+        !this.data.parent && this.setData({
+          parent: target
+        });
+        // 每次被插入到custom-ul时执行，target是custom-ul节点实例对象，触发在attached生命周期之后
+      }
     },
-    '../combined-tabs/index': {
-      type: 'parent'
+    "../combined-tabs/index": {
+      type: "parent",
+      linked: function(target) {
+        !this.data.parent && this.setData({
+          parent: target
+        });
+        // 每次被插入到custom-ul时执行，target是custom-ul节点实例对象，触发在attached生命周期之后
+      }
     }
   },
   /**
@@ -14,28 +26,38 @@ Component({
    */
   properties: {
     tab: String,
-    subTab:String,
-    subKey:String,
+    subTab: String,
+    subKey: String,
     key: String,
-    icon:String,
-    iconStyle:String,
-    image:Object,
-    picPlacement:{
-      type:String,
-      value:'top'
-    },
+    icon: String,
+    iconStyle: String,
+    image: Object,
+    picPlacement: {
+      type: String,
+      value: "top"
+    }
   },
-  
+
+  observers: {
+    "**": function(filed) {
+      this.updateData(filed);
+    }
+  },
 
   /**
    * 组件的初始数据
    */
   data: {
     isCurrent: false,
-    index:0,
+    index: 0,
+    parent: null
   },
 
-  methods:{
-    
+  methods: {
+    updateData(filed) {
+      let parent = this.data.parent;
+      if (!parent) return;
+      parent.initTabs()
+    }
   }
-})
+});
