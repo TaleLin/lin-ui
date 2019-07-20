03f22bcde3005427b4d224bc9e4bd64ca26a5093
aQingYun <37834281+aQingYun@users.noreply.github.com>
2019-07-20 22:14:57 +0800

    fix:修复按需加载某些情况下缺少common文件的bug (#364)

--- build/build-tool.js
+++ build/build-tool.js
@@ -1,35 +1,35 @@
-const fs = require('fs')
-let componentArr = []
+const fs = require('fs');
+let componentArr = [];
 
-function componentData () {
-  let data = fs.readFileSync(`../config/component.json`, 'utf-8')
-  let params = JSON.parse(data).components
-  componentArr.push(...params)
-  forParams(params)
-  let result = [...new Set(componentArr)].join()
-  return result
-};
+function componentData() {
+  let data = fs.readFileSync(`../config/component.json`, 'utf-8');
+  let params = JSON.parse(data).components;
+  componentArr.push(...params);
+  forParams(params);
+  let result = [...new Set(componentArr)].join();
+  return result;
+}
 
 const forParams = (arr) => {
-  let finishArr = []
+  let finishArr = [];
   arr.map(item => {
-    let data = fs.readFileSync(`../src/${item}/index.json`, 'utf-8')
-    const params = JSON.parse(data)
-    const { usingComponents } = params
+    let data = fs.readFileSync(`../src/${item}/index.json`, 'utf-8');
+    const params = JSON.parse(data);
+    const { usingComponents } = params;
     if (usingComponents && !isEmptyObj(usingComponents)) {
       for (let key in usingComponents) {
-        let keyComponent = key.substring(2, key.length)
-        finishArr.push(keyComponent)
+        let keyComponent = key.substring(2, key.length);
+        finishArr.push(keyComponent);
       }
-      componentArr.push(...finishArr)
-      forParams(finishArr)
+      componentArr.push(...finishArr);
+      forParams(finishArr);
     }
-  })
-}
+  });
+};
 
 const isEmptyObj = (obj) => {
-  let result = (JSON.stringify(obj) === "{}");
-  return result
-}
+  let result = (JSON.stringify(obj) === '{}');
+  return result;
+};
 
 module.exports = componentData;
