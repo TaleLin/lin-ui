8b75a7ba0e6434ab992b84616cbaa1aed9df9269
mario.zhang <525349965@qq.com>
2019-09-08 16:20:03 +0800

    fix/statusShow
    1. 去除part模式
    2. 固定整体宽度为500rpx
    3. 支持wx.lin调用
    4. 解决点击事件出现两次的问题
    5. 增加整体组件的点击事件lincovertap
    6. 增加fullScreen属性

--- src/status-show/index.js
+++ src/status-show/index.js
@@ -3,7 +3,7 @@ Component({
   /**
    * 组件的属性列表
    */
-  externalClasses: ['l-class', 'l-image-class', 'l-button-class', 'l-describe-class', 'l-container-class'],
+  externalClasses: ['l-class', 'l-image-class', 'l-button-class', 'l-describe-class'],
   properties: {
     show: Boolean,
     type: {
@@ -16,23 +16,15 @@ Component({
     buttonText: String,
     bgColor: {
       type: String,
-      valure: '#fff'
+      value: '#fff'
     },
-    top: {
-      type: Number,
-      value: 260,
-    },
-    autoFit: {
+    fullScreen: {
       type: Boolean,
       value: true,
     },
-    buttonColor: {
-      type: String,
-      value: ''
-    },
-    part: {
+    openApi: {
       type: Boolean,
-      value: false
+      value: true
     }
   },
 
@@ -40,150 +32,44 @@ Component({
    * 组件的初始数据
    */
   data: {
-    multiple: 1,
-    image_margin_top: 260,
-    ad_img_width: 198,
-    ad_img_height: 204,
-    top_img_width: 264,
-    top_img_height: 204,
-    left_img_width: 120,
-    left_img_height: 184,
-    text_margin_top: 40,
-    font_size: 30,
-    button_margin_top: 80,
-    button_height: 36,
-    button_width: 90,
+
   },
 
-  lifetimes: {
-    attached: function () {
-      this._initStatusShow();
-      if (this.data.autoFit) {
-        this.autoSetMarginTop();
-      }
-    },
-
-    ready() {
-      this._changeStatus();
-    },
+  attached() {
+    this._changeStatus();
+    if (this.data.openApi) this._init();
   },
 
   pageLifetimes: {
     show() {
-      this._initStatusShow();
-      const that = this;
-
-      wx.getSystemInfo({
-        success(res) {
-          that.setData({
-            windowHeight: res.windowHeight,
-            windowWidth: res.windowWidth,
-            px2rpx: res.windowWidth / 750
-          });
-        }
-      });
-
-      if (!this.data.part) {
-        return;
-      }
-
-      const query = wx.createSelectorQuery().in(this);
-      query.select('#statusShow').boundingClientRect();
-      query.exec(function (res) {
-        const multiple_width = res[0].width / that.data.windowWidth;
-        const multiple_height = res[0].height / that.data.windowHeight;
-        const multiple = multiple_width > multiple_height ? multiple_height : multiple_width;
-        that.setData({
-          statusShow_width: res[0].width,
-          statusShow_height: res[0].height,
-          multiple,
-          ad_img_width: that.data.ad_img_width * multiple,
-          ad_img_height: that.data.ad_img_height * multiple,
-          top_img_width: that.data.top_img_width * multiple,
-          top_img_height: that.data.top_img_height * multiple,
-          left_img_width: that.data.left_img_width * multiple,
-          left_img_height: that.data.left_img_height * multiple,
-          top: that.data.top * multiple,
-          text_margin_top: that.data.text_margin_top * multiple,
-          font_size: that.data.font_size * multiple > 16 ? that.data.font_size * multiple : 16,
-          button_margin_top: that.data.button_margin_top * multiple,
-        });
-      });
-
-      const query_button = wx.createSelectorQuery().in(this);
-      query_button.select('#button').boundingClientRect();
-      query_button.exec(function (res) {
-        if (res[0]) {
-          that.setData({
-            button_height: that.data.button_height * that.data.multiple > 28 ? that.data.button_height * that.data.multiple : 28,
-            button_width: that.data.button_width * that.data.multiple,
-          });
-        }
-      });
+      this._init();
     },
-  },
-
-  observers: {
-    'type, statusShow_height, text_margin_top, button_height, button_margin_top': function () {
-      if (this.data.part) {
-        const image_height = this.data.type == 'success' || this.data.type == 'error' ? this.data.left_img_height : this.data.type == 'address' ? this.data.ad_img_height : this.data.top_img_height;
-        const button_height = this.data.buttonText || this.data.type == 'cart' || this.data.type == 'network' ? 44 : 0;
-        const button_margin_top = this.data.buttonText || this.data.type == 'cart' || this.data.type == 'network' ? this.data.button_margin_top : 0;
-        const top = (this.data.statusShow_height / this.data.px2rpx - image_height - this.data.text_margin_top - button_height - button_margin_top - 44) / 2;
-        this.setData({
-          top
-        });
-      }
-    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    _initStatusShow() {
+    _init() {
       wx.lin = wx.lin || {};
-      wx.lin.showStatusShow = (options = {}) => {
+      wx.lin.showStatusShow = (options) => {
         const {
           type = 'success',
           image = '',
           describe = '',
           buttonText = '',
-          bgColor = 'fff',
-          top = 260,
-          autoFit = true,
-          buttonColor = '',
-          part = false,
-        } = options;
-        if (autoFit) {
-          this.setData({
-            type,
-            image,
-            describe,
-            buttonText,
-            bgColor,
-            autoFit,
-            buttonColor,
-            show: true,
-            part,
-          });
-        } else {
-          this.setData({
-            type,
-            image,
-            describe,
-            buttonText,
-            bgColor,
-            autoFit,
-            top,
-            buttonColor,
-            show: true,
-            part,
-          });
-        }
-
-        this._changeStatus();
-        return this;
+          bgColor = '#fff',
+          fullScreen = true
+        } = { ...options };
+        this.setData({
+          show: true,
+          type,
+          image,
+          describe,
+          buttonText,
+          bgColor,
+          fullScreen
+        });
       };
       wx.lin.hideStatusShow = () => {
         this.setData({
@@ -191,28 +77,7 @@ Component({
         });
       };
     },
-
-    autoSetMarginTop() {
-      const that = this;
-      const image_height = this.data.type == 'success' || this.data.type == 'error' ? this.data.left_img_height : this.data.type == 'address' ? this.data.ad_img_height : this.data.top_img_height;
-      const button_height = this.data.buttonText || this.data.type == 'cart' || this.data.type == 'network' ? 72 : 0;
-      const button_margin_top = this.data.buttonText || this.data.type == 'cart' || this.data.type == 'network' ? 80 : 0;
-      wx.getSystemInfo({
-        success(res) {
-          const multiple = res.windowWidth / 750;
-          // 屏幕居中
-          const top = (res.screenHeight / multiple - image_height - 40 - 84 - button_height - button_margin_top) / 2 + res.windowHeight / multiple - res.screenHeight / multiple;
-          // 可视区域居中
-          // const top = (res.windowHeight * 750 / res.windowWidth - image_height - 40 - 84 - button_height - button_margin_top) / 2 
-
-          that.setData({
-            top: top
-          });
-
-        }
-      });
-    },
-
+    
     _changeStatus() {
       switch (this.properties.type) {
       case 'success':
@@ -271,20 +136,6 @@ Component({
         bubbles: true,
         composed: true
       });
-    },
-
-    loadimage(e) {
-      this.setData({
-        customImageHeight: e.detail.height,
-        customImageWidth: e.detail.width
-      });
-      if (this.data.part) {
-        this.setData({
-          customImageHeight: this.data.customImageHeight * this.data.multiple,
-          customImageWidth: this.data.customImageWidth * this.data.multiple
-        });
-      }
     }
-
   }
 });
