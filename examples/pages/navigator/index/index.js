0e5d91d594b2c2a92123065d39319d04db3624e4
张寸起 <zhangcunqi@zhangcunqideMacBook-Pro.local>
2019-09-29 17:24:28 +0800

    fix:更改utils文件

--- examples/pages/navigator/index/index.js
+++ examples/pages/navigator/index/index.js
@@ -1,11 +1,13 @@
 //index.js
 import naviConfigs from './navi.js';
+import { promisic } from '../../../dist/utils/util.js'
 Page({
   data: {
     naviConfigs: naviConfigs
   },
 
   onLoad: function() {
+    console.log(promisic)
   },
 
   onShareAppMessage() {
