2e8d2fe4c69ce34ff7b3f6a1856ba52de3aeac2d
zhc525349965 <43640797+zhc525349965@users.noreply.github.com>
2019-10-09 21:44:00 +0800

    feat:urls支持传入属性key (#529)
     feat:urls支持传入属性key

--- examples/dist/image-picker/index.js
+++ examples/dist/image-picker/index.js
@@ -60,6 +60,19 @@ Component({
     tempFilePath: '',
   },
 
+  lifetimes: {
+    attached: function () {
+      // 在组件实例进入页面节点树时执行
+      const newOrOld = this.judgeNewOrOld();
+      this.setData({
+        newOrOld
+      });
+      if (newOrOld == 'old') {
+        console.warn('image-picker组件已经升级，建议使用最新版本，当前用法会在后续版本中暂停支持');
+      }
+    },
+  },
+
   /**
    * 组件的方法列表
    */
@@ -77,19 +90,34 @@ Component({
 
     // 预览 preview
     onPreviewTap(e) {
-      const that = this;
       const index = e.currentTarget.dataset.index;
-      const tempFilePath = this.data.urls[index];
+      const urls = this.data.urls;
+      var tempFilePath = '';
+      var previewImageList = [];
+      const newOrOld = this.data.newOrOld;
+
+      if (newOrOld == 'old') {
+        tempFilePath = this.data.urls[index];
+        previewImageList = this.data.urls;
+
+      } else {
+        tempFilePath = this.data.urls[index].url;
+        for (var i = 0; i < urls.length; i++) {
+          previewImageList.push(urls[i].url);
+        }
+      }
+
+
       let detail = {
         index, // 下标
-        current: tempFilePath, // 当前显示图片的http链接
-        all: that.data.urls // 需要预览的图片http链接列表
+        current: urls[index], // 当前显示图片的http链接
+        all: urls // 需要预览的图片http链接列表
       };
       let option = {};
       if (this.data.isPreview === true) {
         wx.previewImage({
           current: tempFilePath, // 当前显示图片的http链接
-          urls: that.data.urls // 需要预览的图片http链接列表
+          urls: previewImageList // 需要预览的图片http链接列表
         });
       }
       this.triggerEvent('linpreview', detail, option);
@@ -100,16 +128,27 @@ Component({
     onAddTap() {
       const that = this;
       const count = this.data.count - this.data.urls.length;
-      if (count === 0 ) {
+      if (count === 0) {
         return;
       }
+      const newOrOld = this.data.newOrOld;
       wx.chooseImage({
         count,
         sizeType: this.data.sizeType,
         sourceType: ['album', 'camera'],
         success(res) {
           // tempFilePath可以作为img标签的src属性显示图片
-          const tempFilePath = res.tempFilePaths;
+          var tempFilePath = [];
+          if (newOrOld == 'old') {
+            tempFilePath = res.tempFilePaths;
+          } else {
+            for (var i = 0; i < res.tempFilePaths.length; i++) {
+              tempFilePath.push({
+                url: res.tempFilePaths[i],
+                // key: null
+              });
+            }
+          }
           const newtempFilePaths = that.data.urls.concat(tempFilePath);
           // 判断是否还能继续添加图片 
           if (newtempFilePaths.length === parseInt(that.data.count)) {
@@ -127,7 +166,7 @@ Component({
             all: newtempFilePaths
           };
           let option = {};
-    
+
           that.triggerEvent('linchange', detail, option);
         }
       });
@@ -155,19 +194,27 @@ Component({
         index,
         current: tempFilePath,
         all: tempFilePaths
-      }; 
+      };
       let option = {};
 
       this.triggerEvent('linremove', detail, option);
 
     },
     handleSplice(arr, current) {
-      const newArr = arr.filter(item => item!== current);
+      const newArr = arr.filter(item => item !== current);
       return newArr;
     },
-    
-  },
 
-  attached: function () {
+    judgeNewOrOld: function () {
+      const urls = this.data.urls;
+      if (urls.length != 0) {
+        if (typeof (urls[0]) != 'object') {
+          return 'old';
+        }
+        return 'new';
+      }
+      return 'new';
+    }
+
   },
 });
