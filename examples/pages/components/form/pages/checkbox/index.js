6ff0ce83b7023670586a97f064cb562f23ca195d
daipengpeng <16602124116@163.com>
2019-10-13 16:12:02 +0800

    fix: 修改checkbox使用案例；修改checkbox按钮默认颜色

--- examples/pages/components/form/pages/checkbox/index.js
+++ examples/pages/components/form/pages/checkbox/index.js
@@ -6,62 +6,192 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    items1: [{
+      id: 1,
+      name: '青花瓷',
+      checked: true
+    }, {
+      id: 2,
+      name: '双截棍',
+      checked: false
+    }, {
+      id: 3,
+      name: '一千年以后',
+      checked: false
+    }, {
+      id: 4,
+      name: '江南',
+      checked: true
+    }],
+    items2: [{
+      id: 1,
+      name: '功夫',
+      checked: false
+    }, {
+      id: 2,
+      name: '喜剧之王',
+      checked: false
+    }, {
+      id: 3,
+      name: '少林足球',
+      checked: false
+    }],
+    items3: [{
+      id: 1,
+      name: '春天',
+      checked: false
+    }, {
+      id: 2,
+      name: '夏天',
+      checked: false
+    }, {
+      id: 3,
+      name: '秋天',
+      checked: false
+    }, {
+      id: 4,
+      name: '冬天',
+      checked: false
+    }],
+    items4: [{
+      id: 1,
+      name: '单刀赴会',
+      checked: true
+    }, {
+      id: 2,
+      name: '大意失荆州',
+      checked: false
+    }, {
+      id: 3,
+      name: '水淹七军',
+      checked: false
+    }],
+    items5: [{
+      id: 1,
+      name: '日温差大',
+      checked: true
+    }, {
+      id: 2,
+      name: '年降雨量小',
+      checked: false
+    }, {
+      id: 3,
+      name: '年温差小',
+      checked: false
+    }, {
+      id: 4,
+      name: '植物根长小',
+      checked: false
+    }],
+    items6: [{
+      id: 1,
+      name: '潘森',
+      checked: true
+    }, {
+      id: 2,
+      name: '提莫',
+      checked: false
+    }, {
+      id: 3,
+      name: '孙悟空',
+      checked: false
+    }],
+    items7: [{
+      id: 1,
+      name: '在任何情况下都可以开启，不会被敌方打断',
+      checked: true
+    }, {
+      id: 2,
+      name: '在开启过程中，无法对敌方进行攻击，但是可以移动',
+      checked: false,
+      disabled: true
+    }, {
+      id: 3,
+      name: '开启后只能免疫技能攻击，无法免疫普通攻击',
+      checked: false
+    }],
+    items8: [{
+      id: 1,
+      name: 'JavaScript',
+      checked: false
+    }, {
+      id: 2,
+      name: 'CSS',
+      checked: true
+    }, {
+      id: 3,
+      name: 'NodeJS',
+      checked: false
+    }, {
+      id: 4,
+      name: 'Webpack',
+      checked: false
+    }],
+    items9: [{
+      id: 1,
+      name: '篮球',
+      checked: true
+    }, {
+      id: 2,
+      name: '排球',
+      checked: false
+    }, {
+      id: 3,
+      name: '足球',
+      checked: false
+    }],
+    items10: [{
+      id: 1,
+      name: '成龙',
+      checked: true
+    }, {
+      id: 2,
+      name: '甄子丹',
+      checked: false
+    }, {
+      id: 3,
+      name: '周润发',
+      checked: false
+    }, {
+      id: 4,
+      name: '周星驰',
+      checked: false
+    }],
+    position: 'left'
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function() {
-
+  change(e) {
+    let index = e.currentTarget.dataset['index'];
+    let items = this.data[`items${index}`];
+    items.forEach(item => {
+      if(item.name === e.detail.key) {
+        item.checked = e.detail.checked;
+      }
+    });
+    this.setData({
+      [`items${index}`]: items
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
+  change2(e) {
+    let index = e.currentTarget.dataset['index'];
+    let items = this.data[`items${index}`];
+    items.forEach(item => {
+      if(item.id == e.detail.key) {
+        item.checked = e.detail.checked;
+      }
+    });
+    this.setData({
+      [`items${index}`]: items
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
+  tipOverflow(data) {
+    let title = data.detail.type === 'overflow_max' ? `最多选择${data.detail.number}个` : `至少选择${data.detail.number}个`;
+    wx.showToast({
+      title: title,
+      icon: 'none',
+      duration: 2000
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
 });
