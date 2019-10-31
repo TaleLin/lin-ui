361352b64bddb692d2bb4d29c52b8ce2a8f497c3
daipengpeng <16602124116@163.com>
2019-10-31 15:52:11 +0800

    feat: 重构form

---
+++ src/form-item/index.js
@@ -0,0 +1,27 @@
+Component({
+  /**
+     * 组件的属性列表
+     */
+  externalClasses: [],
+  properties: {
+    label: String,
+    position: {
+      type: String,
+      value: 'row'
+    }
+  },
+
+  /**
+     * 组件的初始数据
+     */
+  data: {
+
+  },
+
+  /**
+     * 组件的方法列表
+     */
+  methods: {
+
+  }
+});
