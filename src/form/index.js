45b3c6b4fe8cb84e9591dc4e9e41fabda0fb54c5
daipengpeng <16602124116@163.com>
2019-11-27 20:47:09 +0800

    feat:完善表单组件

--- src/form/index.js
+++ src/form/index.js
@@ -103,8 +103,11 @@ Component({
       return currentTarget.data.errors;
     },
 
-    _forEachNodes(func) {
-      const items = this.getRelationNodes('../form-item/index');
+    _forEachNodes(func, isReverse) {
+      let items = this.getRelationNodes('../form-item/index');
+      if (isReverse) {
+        items.reverse()
+      }
       items.forEach((item, index) => {
         func(item, index);
       });
@@ -125,7 +128,7 @@ Component({
         }
         this._errors[id] = item.data.errors;
         formErrors = formErrors.concat(item.data.errors);
-      });
+      }, true);
       return formErrors;
     },
 
