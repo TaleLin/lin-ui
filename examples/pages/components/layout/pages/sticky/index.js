d4cd817eedcd784dc5376f220e937ba0f798760c
桔子 <juzi214032@qq.com>
2020-02-16 20:57:46 +0800

    react:重构sticky组件示例

--- examples/pages/components/layout/pages/sticky/index.js
+++ examples/pages/components/layout/pages/sticky/index.js
@@ -5,80 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    scrollTop: 0,
-    addresses:[
-      {
-        index:"A",
-        items:[
-          "阿坝",
-          "阿克苏",
-        ]
-      },
-      {
-        index:"B",
-        items:[
-          "百色",
-          "白沙",
-        ]
-      },
-      {
-        index:"C",
-        items:[
-          "重庆",
-          "长白山",
-        ]
-      }
-    ],
-    grids2: [{
-      image: 'cart',
-      text: '我的购物车'
-    }, {
-      image: 'help',
-      text: '帮助中心'
-    }, {
-      image: 'address',
-      text: '地址管理'
-    }, {
-      image: 'order',
-      text: '我的订单'
-    }, {
-      image: 'customer-service',
-      text: '联系客服'
-    },],
-    dynamicCard:[
-      {
-        title:"动态插入数据示例",
-        content:"点击下面按钮动态插入一条数据，模拟上拉加载更多的业务场景"
-      }
-    ]
+
   },
 
-  onPageScroll(e) {
-    this.setData({
-      scrollTop: e.scrollTop
+  onTapBase(){
+    console.log("进入")
+    wx.navigateTo({
+      url:"/pages/components/layout/pages/sticky/pages/sticky-base/index"
     })
   },
 
-  onAddData(){
-    let size = this.data.dynamicCard.length
-    let tempData = this.data.dynamicCard
-    let data = {
-      title:`这是第${size+1}条数据`,
-      content:`这是动态增加的第${size+1}条数据。点击下面按钮动态插入一条数据，模拟上拉加载更多的业务场景`
-    }
-    tempData.push(data)
-    this.setData({
-      dynamicCard:tempData
+  onTapAppoint(){
+    wx.navigateTo({
+      url:"/pages/components/layout/pages/sticky/pages/sticky-appoint/index"
     })
-
-    wx.lin.flushSticky()
   },
 
-  linunsticky(){
-    wx.showToast({title:"容器脱落！"})
-  },
-
-  linsticky(){
-    wx.showToast({title:"容器吸附！"})
+  onTapDynamic(){
+    wx.navigateTo({
+      url:"/pages/components/layout/pages/sticky/pages/sticky-dynamic/index"
+    })
   }
+
 })
