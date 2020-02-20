8c16a6fdad1b8ac5b3176985418bcd0c9534e800
林白 <499055803@qq.com>
2020-02-20 22:37:18 +0800

    feat:完善构建过程 (#694)
    * feat(button):新增l-label-class外部样式类
    
    * feat(example):清除示例代码wx:for的warning
    
    * feat(package.json):更改homepage地址
    
    * feat(build):更改husky信息
    
    * feat(package.json): 增加husky校验commit

--- dist/behaviors/zIndex.js
+++ dist/behaviors/zIndex.js
@@ -1,10 +1,1 @@
-// eslint-disable-next-line no-undef
-export default Behavior({
-  behaviors: [],
-  properties: {
-    zIndex:{
-      type: Number,
-      value: 777
-    }
-  }
-});
+export default Behavior({behaviors:[],properties:{zIndex:{type:Number,value:777}}});
--- dist/countdown/index.js
+++ dist/countdown/index.js
@@ -1,18 +1,1 @@
-import countDownBehavios from '../behaviors/countdown';
-Component({
-  externalClasses: [
-    'l-class',
-    'l-class-time',
-    'l-time-class'
-  ],
-  behaviors:[countDownBehavios],
-  properties: {
-    doneText:{
-      type:String,
-      value:'已结束'
-    }
-  },
-  methods: {
-        
-  }
-});
+import countDownBehavios from"../behaviors/countdown";Component({externalClasses:["l-class","l-class-time","l-time-class"],behaviors:[countDownBehavios],properties:{doneText:{type:String,value:"已结束"}},methods:{}});
