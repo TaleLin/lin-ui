1f09212724720f3b5f1452eaf3d098553a38959b
桔子 <38206169+juzi214032@users.noreply.github.com>
2020-02-27 10:38:33 +0800

    react:重构sticky组件示例
    * feat:新增吸顶容器 Sticky组件
    
    * refact:将top属性的单位修改为rpx
    
    * react:重构sticky组件示例

---
+++ examples/pages/components/layout/pages/sticky/pages/sticky-appoint/index.js
@@ -0,0 +1,70 @@
+// pages/components/layout/pages/sticky/pages/sticky-appoint/index.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    grids2: [
+      {
+        image: "cart",
+        text: "我的购物车"
+      },
+      {
+        image: "help",
+        text: "帮助中心"
+      },
+      {
+        image: "address",
+        text: "地址管理"
+      },
+      {
+        image: "order",
+        text: "我的订单"
+      },
+      {
+        image: "customer-service",
+        text: "联系客服"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {}
+})
