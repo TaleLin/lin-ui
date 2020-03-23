23ce0d43554199f718551aeece718c30d5219126
桔子 <juzi214032@qq.com>
2020-03-23 12:05:04 +0800

    feat(collapse): 实现组件基本功能

---
+++ examples/dist/collapse-item/index.js
@@ -0,0 +1,39 @@
+Component({
+  properties: {},
+  data: {
+    /**
+     * 内容区高度
+     */
+    bodyHeight: '0',
+    /**
+     * 内容区是否展开
+     */
+    bodyExpand: false
+  },
+  methods: {
+    /**
+     * 点击标题
+     */
+    onTapTitle() {
+      if (this.data.bodyExpand) {
+        this.setData({
+          bodyExpand: !this.data.bodyExpand,
+          bodyHeight: 0
+        })
+        return
+      }
+      wx.createSelectorQuery()
+        .in(this)
+        .select(".container-body-wrapper")
+        .fields({size: true}, (res) => {
+          this.setData({
+            bodyExpand: !this.data.bodyExpand,
+            bodyHeight: res.height
+          })
+        })
+        .exec()
+
+
+    }
+  }
+});
---
+++ src/collapse/index.js
@@ -0,0 +1,5 @@
+Component({
+  properties: {},
+  data: {},
+  methods: {}
+});
