a730f70091698ff92fef42a9ad6dd6e7f139072a
桔子 <juzi214032@qq.com>
2020-03-25 20:07:18 +0800

    feat(collapse): 完成Collapse组件使用案例

--- src/collapse-item/index.js
+++ src/collapse-item/index.js
@@ -1,5 +1,45 @@
 Component({
-  properties: {},
+
+  externalClasses:['l-class','l-title-class','l-body-class'],
+
+  relations: {
+    '../collapse/index': {
+      type: 'parent'
+    }
+  },
+
+  options: {
+    multipleSlots: true
+  },
+
+  properties: {
+    title: {
+      type: String,
+      value: '默认标题'
+    },
+    /**
+     * 自定义标题部分内容
+     */
+    customTitle: {
+      type: Boolean,
+      value: false
+    },
+    /**
+     * 是否禁用内容区展开
+     */
+    disable: {
+      type: Boolean,
+      value: false
+    },
+    /**
+     * 是否显示分隔线
+     */
+    divide: {
+      type: Boolean,
+      value: true
+    }
+
+  },
   data: {
     /**
      * 内容区高度
@@ -8,32 +48,46 @@ Component({
     /**
      * 内容区是否展开
      */
-    bodyExpand: false
+    isExpandContent: false,
+
   },
   methods: {
     /**
      * 点击标题
      */
     onTapTitle() {
-      if (this.data.bodyExpand) {
-        this.setData({
-          bodyExpand: !this.data.bodyExpand,
-          bodyHeight: 0
-        });
+
+      if (this.data.disable) {
         return;
       }
-      wx.createSelectorQuery()
-        .in(this)
+
+      let parents = this.getRelationNodes('../collapse/index');
+      parents[0].onTapCollapseItem(this);
+    },
+
+    /**
+     * 折叠内容区
+     */
+    foldContent() {
+      this.setData({
+        isExpandContent: false,
+        bodyHeight: 0
+      });
+    },
+
+    /**
+     * 展开内容区
+     */
+    expandContent() {
+      this.createSelectorQuery()
         .select('.container-body-wrapper')
         .fields({size: true}, (res) => {
           this.setData({
-            bodyExpand: !this.data.bodyExpand,
+            isExpandContent: true,
             bodyHeight: res.height
           });
         })
         .exec();
-
-
     }
   }
 });
--- src/collapse/index.js
+++ src/collapse/index.js
@@ -1,5 +1,104 @@
 Component({
-  properties: {},
-  data: {},
-  methods: {}
+
+  options: {
+    pureDataPattern: /^_/
+  },
+
+  relations: {
+    '../collapse-item/index': {
+      type: 'child'
+    }
+  },
+
+  lifetimes: {
+    ready: function () {
+      this.updateView();
+    }
+  },
+
+  properties: {
+    /**
+     * 折叠面板类型
+     */
+    type: {
+      type: String,
+      value: 'normal'
+    },
+
+    /**
+     * 需要展开的 collapse-item 的id
+     */
+    expandItemId: {
+      type: Array,
+      value: []
+    }
+  },
+  data: {
+    /**
+     * 当前展开的collapse-item节点列表
+     */
+    _expandItems: []
+  },
+  observers: {
+    'expandItemId': function () {
+      this.updateView();
+    }
+  },
+  methods: {
+    updateView() {
+      let expandItemId;
+      if (this.data.type === 'accordion') {
+        expandItemId = this.data.expandItemId.slice(0, 1);
+      } else {
+        expandItemId = this.data.expandItemId;
+      }
+
+      let children = this.getRelationNodes('../collapse-item/index');
+      for (let i = 0; i < children.length; i++) {
+        let item = children[i];
+        let id = item.id ? item.id : i;
+        if (expandItemId.indexOf(id) > -1 && !item.isExpandContent) {
+          this.setCollapseItemStatus(item, true);
+        } else if (item.isExpandContent || this.data.type === 'accordion') {
+          this.setCollapseItemStatus(item, false);
+        }
+      }
+    },
+
+    onTapCollapseItem(collapseItem) {
+      if (this.data.type === 'accordion') {
+        this.foldAllExpandItem(collapseItem);
+      }
+      this.setCollapseItemStatus(collapseItem,!collapseItem.data.isExpandContent);
+    },
+
+    /**
+     * 设置子组件状态
+     */
+    setCollapseItemStatus(collapseItem, isExpand) {
+      if (isExpand) {
+        collapseItem.expandContent();
+        this.data._expandItems.push(collapseItem);
+      } else {
+        collapseItem.foldContent();
+        for (let i = 0; i < this.data._expandItems.length; i++) {
+          if (this.data._expandItems[i] === collapseItem) {
+            this.data._expandItems.splice(i, 1);
+          }
+        }
+      }
+    },
+
+    /**
+     * 关闭所有打开的collapse-item
+     */
+    foldAllExpandItem(collapseItem) {
+      for (let i = 0; i < this.data._expandItems.length; i++) {
+        if (collapseItem !== this.data._expandItems[i]) {
+          this.data._expandItems[i].foldContent();
+        }
+      }
+      this.data._expandItems = [];
+    }
+  }
 });
