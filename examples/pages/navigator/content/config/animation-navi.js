b6b8d3f4b31ae5d7f23eda0b735ebc5b33821592
桔子 <juzi214032@qq.com>
2020-06-10 12:57:20 +0800

    chore(EsLint): 删除不需要的 env

--- examples/pages/navigator/content/config/animation-navi.js
+++ examples/pages/navigator/content/config/animation-navi.js
@@ -5,7 +5,7 @@ const animationNaviConfigs = [
     title: 'Transition',
     desc: '过渡',
     componentsPath: '/pages/components/animation/pages/transition/index'
-  },ator/content/config/form-navi.js
+  }
 ];
 
 export default animationNaviConfigs;
