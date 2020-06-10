1036d8d5d91d77c5153b45bb48be9f932ad04d6b
桔子 <juzi214032@qq.com>
2020-06-11 01:36:51 +0800

    style(EsLint): 格式化部分 js 代码

--- examples/pages/components/nav/pages/capsule-bar/index.js
+++ examples/pages/components/nav/pages/capsule-bar/index.js
@@ -1,5 +1,3 @@
-import navigationBarUtil from '../../../../../dist/navigation-bar/navigation-bar-util';
-
 Page({
 
   /**
@@ -10,8 +8,8 @@ Page({
     starCount: null
   },
 
-  onLoad(query) {
-    this.getStarCount()
+  onLoad() {
+    this.getStarCount();
   },
 
   onShareAppMessage: function () {
@@ -22,56 +20,56 @@ Page({
    * 获取 Star 数量，有些网络无法访问，暂时不用
    */
   getStarCount() {
-    const that = this
+    const that = this;
     wx.request({
       url: 'https://api.github.com/repos/TaleLin/lin-ui',
       success(res) {
-        let starCount = res.data.stargazers_count
-        starCount = (starCount / 1000).toFixed(1)
-        that.setData({starCount})
+        let starCount = res.data.stargazers_count;
+        starCount = (starCount / 1000).toFixed(1);
+        that.setData({starCount});
       }
-    })
+    });
   },
 
   /**
    * 监听：长按左侧按钮
    */
   onLongPressLeft() {
-    wx.vibrateShort()
+    wx.vibrateShort();
     wx.showModal({
       title: '提示',
       content: '长按左侧按钮事件被触发'
-    })
+    });
   },
 
   /**
    * 监听：长按右侧按钮
    */
   onLongPressRight() {
-    wx.vibrateShort()
+    wx.vibrateShort();
     wx.showModal({
       title: '提示',
       content: '长按右侧按钮事件被触发'
-    })
+    });
   },
 
   /**
    * 监听：点击 Star 卡片
    */
   onTapStarCard() {
-    wx.vibrateShort()
+    wx.vibrateShort();
     wx.setClipboardData({
       data: 'https://github.com/TaleLin/lin-ui'
-    })
+    });
   },
 
   /**
    * 监听：点击公众号卡片
    */
   onTapPublicCard() {
-    wx.vibrateShort()
+    wx.vibrateShort();
     wx.setClipboardData({
       data: '林间有风'
-    })
+    });
   }
-})
+});
--- src/utils/device-util.js
+++ src/utils/device-util.js
@@ -9,7 +9,7 @@ class DeviceUtil {
    * @param systemInfo 设备信息
    */
   constructor(systemInfo) {
-    this.systemInfo = systemInfo
+    this.systemInfo = systemInfo;
   }
 
   /**
@@ -17,8 +17,8 @@ class DeviceUtil {
    * @param px 像素值
    */
   px2rpx(px) {
-    const windowWidth = this.systemInfo.windowWidth
-    return 750 / windowWidth * px
+    const windowWidth = this.systemInfo.windowWidth;
+    return 750 / windowWidth * px;
   }
 
   /**
@@ -26,37 +26,37 @@ class DeviceUtil {
    * @param rpx
    */
   rpx2px(rpx) {
-    const windowWidth = this.systemInfo.windowWidth
-    return windowWidth / 750 * rpx
+    const windowWidth = this.systemInfo.windowWidth;
+    return windowWidth / 750 * rpx;
   }
 
   /**
    * 获取导航栏高度（单位rpx）
    */
   getNavigationBarHeight() {
-    const titleBarHeight = this.getTitleBarHeight()
-    const statusBarHeight = this.getStatusBarHeight()
-    return titleBarHeight + statusBarHeight
+    const titleBarHeight = this.getTitleBarHeight();
+    const statusBarHeight = this.getStatusBarHeight();
+    return titleBarHeight + statusBarHeight;
   }
 
   /**
    * 获取状态栏高度（单位rpx）
    */
   getStatusBarHeight() {
-    return this.px2rpx(this.systemInfo.statusBarHeight)
+    return this.px2rpx(this.systemInfo.statusBarHeight);
   }
 
   /**
    * 获取标题栏高度（单位rpx）
    */
   getTitleBarHeight() {
-    const statusBarHeight = this.systemInfo.statusBarHeight
-    const capsuleButtonInfo = wx.getMenuButtonBoundingClientRect()
-    const gap = capsuleButtonInfo.top - statusBarHeight
-    return this.px2rpx(gap * 2 + capsuleButtonInfo.height)
+    const statusBarHeight = this.systemInfo.statusBarHeight;
+    const capsuleButtonInfo = wx.getMenuButtonBoundingClientRect();
+    const gap = capsuleButtonInfo.top - statusBarHeight;
+    return this.px2rpx(gap * 2 + capsuleButtonInfo.height);
   }
 
 }
 
-const deviceUtil = new DeviceUtil(wx.getSystemInfoSync())
-export default deviceUtil
+const deviceUtil = new DeviceUtil(wx.getSystemInfoSync());
+export default deviceUtil;
