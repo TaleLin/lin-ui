3db3bc336e8bd7f6a521f293507d441d473903cf
桔子 <juzi214032@qq.com>
2020-06-13 20:56:30 +0800

    refactor(CombinedTabs): 重构 Promise 代码，去除嵌套调用

--- src/combined-tabs/index.js
+++ src/combined-tabs/index.js
@@ -1,11 +1,22 @@
-// components/tabs/index.js
+import nodeUtil from '../core/utils/node-util';
+
 Component({
-  externalClasses: ['l-class-header', 'l-class-active', 'l-class-inactive', 'l-class-line', 'l-class-tabimage',
-    'l-header-class', 'l-active-class', 'l-inactive-class', 'l-line-class', 'l-tabimage-class','l-content-class'
+  externalClasses: [
+    'l-class-header',
+    'l-class-active',
+    'l-class-inactive',
+    'l-class-line',
+    'l-class-tabimage',
+    'l-header-class',
+    'l-active-class',
+    'l-inactive-class',
+    'l-line-class',
+    'l-tabimage-class',
+    'l-content-class'
   ],
   relations: {
     '../tabpanel/index': {
-      type: 'child',
+      type: 'child'
     },
     linked() {
       // 每次有子节点被插入时执行，target是该节点实例对象，触发在该节点attached生命周期之后
@@ -26,13 +37,13 @@ Component({
     },
     placement: {
       type: String,
-      value: 'top',
+      value: 'top'
     },
     aminmated: Boolean,
     scrollable: Boolean,
     swipeable: {
       type: Boolean,
-      value: true,
+      value: true
     },
     hasLine: {
       type: Boolean,
@@ -45,22 +56,19 @@ Component({
     inactiveColor: {
       type: String,
       value: '#bbbbbb'
-    },
-
+    }
   },
 
   data: {
     tabList: [],
     currentIndex: 0,
     transformX: 0,
-    transformY: 0,
+    transformY: 0
   },
 
   ready() {
     this.initTabs();
-
   },
-
 
   /**
    * 组件的方法列表
@@ -77,22 +85,27 @@ Component({
         activeKey = !val && index === 0 ? item.key : activeKey;
         currentIndex = item.key === activeKey ? index : currentIndex;
       });
-      this.setData({
-        activeKey,
-        currentIndex,
-      }, () => {
-        if (this.data.scrollable) {
-          this.queryMultipleNodes();
+      this.setData(
+        {
+          activeKey,
+          currentIndex
+        },
+        () => {
+          if (this.data.scrollable) {
+            this.queryMultipleNodes();
+          }
         }
-      });
+      );
     },
 
     initTabList() {
       let items = this.getRelationNodes('../tabpanel/index');
       if (items.length > 0) {
         const tabList = [];
-        items.forEach((item) => {
-          const tabIndex = tabList.findIndex(tabItem => tabItem.tab === item.data.tab);
+        items.forEach(item => {
+          const tabIndex = tabList.findIndex(
+            tabItem => tabItem.tab === item.data.tab
+          );
           let tab = {};
           if (tabIndex === -1) {
             tab = {
@@ -101,7 +114,7 @@ Component({
               icon: item.data.icon,
               iconStyle: item.data.iconStyle,
               image: item.data.image,
-              subTabs: [],
+              subTabs: []
             };
             tabList.push(tab);
           }
@@ -110,23 +123,21 @@ Component({
             targetTab.subTabs = targetTab.subTabs || [];
             const subTabItem = {
               tab: item.data.subTab,
-              key: item.data.subKey,
+              key: item.data.subKey
             };
             targetTab.subTabs.push(subTabItem);
-            targetTab.activeSubKey = this.data.subActiveKey || targetTab.subTabs[0].key;
+            targetTab.activeSubKey =
+              this.data.subActiveKey || targetTab.subTabs[0].key;
             targetTab.subCurrentIndex = 0;
           }
         });
         this.setData({
-          tabList,
+          tabList
         });
       }
     },
     swiperChange(e) {
-      const {
-        source,
-        current
-      } = e.detail;
+      const {source, current} = e.detail;
       if (source === 'touch') {
         const currentIndex = current;
         const activeKey = this.data.tabList[current].key;
@@ -136,22 +147,18 @@ Component({
           activeKey,
           currentIndex,
           subCurrentIndex,
-          activeSubKey,
+          activeSubKey
         });
       }
     },
     subSwiperChange(e) {
-      const {
-        source,
-        current
-      } = e.detail;
+      const {source, current} = e.detail;
       if (source === 'touch') {
-        const {
-          currentIndex,
-          activeKey
-        } = this.data;
+        const {currentIndex, activeKey} = this.data;
         const subCurrentIndex = current;
-        const activeSubKey = this.data.tabList[currentIndex].subTabs[subCurrentIndex].key;
+        const activeSubKey = this.data.tabList[currentIndex].subTabs[
+          subCurrentIndex
+        ].key;
         const tabs = this.data.tabList[currentIndex];
         tabs.activeSubKey = activeSubKey;
         tabs.subCurrentIndex = subCurrentIndex;
@@ -170,14 +177,15 @@ Component({
     },
     handleChange(e) {
       const isSubHeader = e.currentTarget.dataset.headerType === 'subTab';
-      const {
-        currentIndex,
-        activeKey
-      } = this.data;
+      const {currentIndex, activeKey} = this.data;
 
       const clickIndex = e.currentTarget.dataset.index;
-      const subCurrentIndex = isSubHeader ? clickIndex : this.data.tabList[clickIndex].subCurrentIndex;
-      const activeSubKey = isSubHeader ? this.data.tabList[currentIndex].subTabs[subCurrentIndex].key : this.data.tabList[clickIndex].activeSubKey;
+      const subCurrentIndex = isSubHeader
+        ? clickIndex
+        : this.data.tabList[clickIndex].subCurrentIndex;
+      const activeSubKey = isSubHeader
+        ? this.data.tabList[currentIndex].subTabs[subCurrentIndex].key
+        : this.data.tabList[clickIndex].activeSubKey;
       if (isSubHeader) {
         const tabs = this.data.tabList[currentIndex];
         tabs.activeSubKey = activeSubKey;
@@ -191,7 +199,6 @@ Component({
           activeSubKey,
           subCurrentIndex
         });
-
       } else {
         const activeKey = e.currentTarget.dataset.key;
         this._setChangeData({
@@ -207,62 +214,43 @@ Component({
       activeKey,
       currentIndex,
       activeSubKey = '',
-      subCurrentIndex = null,
+      subCurrentIndex = null
     }) {
-      this.setData({
-        activeKey,
-        currentIndex
-      }, () => {
-        if (this.data.scrollable) {
-          this.queryMultipleNodes();
+      this.setData(
+        {
+          activeKey,
+          currentIndex
+        },
+        () => {
+          if (this.data.scrollable) {
+            this.queryMultipleNodes();
+          }
         }
-      });
+      );
       this.triggerEvent('linchange', {
         activeKey,
         currentIndex,
         activeSubKey,
-        subCurrentIndex,
+        subCurrentIndex
       });
     },
 
-    queryMultipleNodes() {
-      const {
-        placement,
-        activeKey,
-        tabList
-      } = this.data;
-      this._getRect('#' + activeKey)
-        .then((res) => {
-          if (['top', 'bottom'].indexOf(placement) !== -1) {
-            this.setData({
-              transformX: res.left - tabList.length / 2 * res.width,
-              transformY: 0
-            });
-          } else {
-            this._getRect('.l-tabs-header')
-              .then((navRect) => {
-                const transformY = res.top - navRect.top - navRect.height / 2;
-                this.setData({
-                  transformX: 0,
-                  transformY: transformY
-                });
-              }).catch(err=>{
-                console.error(err);
-              });
-          }
-        }).catch(err=>{
-          console.error(err);
+    async queryMultipleNodes() {
+      const {placement, activeKey, tabList} = this.data;
+      const res = await nodeUtil.getNodeRectFromComponent(this, '#' + activeKey);
+      if (['top', 'bottom'].indexOf(placement) !== -1) {
+        this.setData({
+          transformX: res.left - (tabList.length / 2) * res.width,
+          transformY: 0
         });
-    },
-
-    _getRect(selector) {
-      return new Promise((resolve, reject) => {
-        const query = wx.createSelectorQuery().in(this);
-        query.select(selector).boundingClientRect((res) => {
-          if (!res) return reject('找不到元素');
-          resolve(res);
-        }).exec();
-      });
+      } else {
+        const navRect = await nodeUtil.getNodeRectFromComponent(this, '.l-tabs-header');
+        const transformY = res.top - navRect.top - navRect.height / 2;
+        this.setData({
+          transformX: 0,
+          transformY: transformY
+        });
+      }
     }
   }
 });
