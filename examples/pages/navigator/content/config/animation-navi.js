7f9aabe39847549f110c644c494ef69a39e6017c
桔子 <juzi214032@qq.com>
2020-08-04 19:34:14 +0800

    refactor(Example): 新增 Transition 组件图标

--- examples/pages/navigator/content/config/animation-navi.js
+++ examples/pages/navigator/content/config/animation-navi.js
@@ -1,7 +1,7 @@
 
 const animationNaviConfigs = [
   {
-    icon: '/images/component/icon.png',
+    icon: 'cloud://env-9eb476.656e-env-9eb476-1258886794/images/components/transition/transition-icon.png',
     title: 'Transition',
     desc: '过渡',
     componentsPath: '/pages/components/animation/pages/transition/index'
