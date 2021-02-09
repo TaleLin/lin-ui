51edbbecd6e7266be21a6012a82205f4428fc5e5
Juzi <juzi214032@qq.com>
2021-02-09 21:52:51 +0800

    build: Travis CI automatic compilation

--- examples/dist/image-picker/index.js
+++ examples/dist/image-picker/index.js
@@ -1,284 +1,310 @@
-import validator from '../behaviors/validator';
+import nodeUtil from '../core/utils/node-util';
+import deviceUtil from '../utils/device-util';
+import eventUtil from '../core/utils/event-util';
+import {promisic} from '../utils/util';
 
 Component({
-  /**
-   * 组件的属性列表
-   */
+
+  options: {
+    // 纯数据字段
+    pureDataPattern: /urls|cells|preview|remove|sizeType|maxImageSize|clear/
+  },
+  behaviors: ['wx://form-field'],
   externalClasses: ['l-class', 'l-item-class'],
-  behaviors: ['wx://form-field', validator],
   properties: {
+    // 图片 url 数组，与 cells 二选一传入
     urls: {
       type: Array,
-      value: []
+      value: null
     },
-    // 最多可以选择的图片张数
-    count: {
-      type: [String, Number],
-      value: 9
-    },
-    // 清除urls
-    clear: {
-      type: Boolean,
-      value: false,
-      observer: function (newVal) {
-        if (newVal) {
-          this.handleClear();
-        }
-      }
+    // 图片 url 对象数组，与 urls 二选一传入
+    cells: {
+      type: Array,
+      value: null
     },
     // 每行可显示的个数
     size: {
-      type: [String, Number],
-      value: 3,
-      options: [3, 4, '3', '4']
+      type: Number,
+      value: null
     },
-    // 所选的图片的尺寸 ['original', 'compressed']
-    sizeType: {
-      type: String,
-      value: 'original',
-      options: ['original', 'compressed']
-    },
-    // 图片裁剪、缩放的模式
+    // 图片显示模式
     mode: {
       type: String,
-      value: 'aspectFit', // 参考微信小程序 image 组件的mode属性列表
-      options: ['scaleToFill', 'aspectFit', 'aspectFill', 'widthFix', 'top', 'bottom', 'center', 'left', 'right', 'top left', 'top right', 'bottom left', 'bottom right']
+      value: 'aspectFit',
     },
-    // 设置是否传入slot
-    custom: {
-      type: Boolean,
-      value: false
-    },
-    // 是否可以预览
+    // 图片是否可预览
     preview: {
       type: Boolean,
       value: true
     },
+    // 图片是否可被删除
+    remove: {
+      type: Boolean,
+      value: true
+    },
+    // 最大可选择图片数量
+    count: {
+      type: Number,
+      value: 9
+    },
+    sizeType: {
+      // 该写法经测试有效
+      type: Array|String,
+      value: ['original', 'compressed']
+    },
     // 所选图片最大限制，单位字节
     maxImageSize: {
       type: Number,
-      value: 10000000,
+      value: 0,
     },
-    // 以对象方式传入图片链接
-    cells: {
-      type: Array,
-      value: null
-    }
+    // 清除 value
+    clear: {
+      type: Boolean,
+      value: false
+    },
+    // 是否传入自定义 slot
+    // 已弃用，直接传入 slot 即可，无需修改该属性
+    // todo 1.0.0 移除
+    custom: {
+      type: Boolean,
+      value: false
+    },
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
-    showBtn: true,
-    tempFilePath: '',
-  },
-
-  lifetimes: {
-    attached: function () {
-      // 在组件实例进入页面节点树时执行
-      let newOrOld = this.judgeNewOrOld();
-      // 对 cells 的兼容处理
-      if (this.data.cells !== null) {
-        newOrOld = 'new';
-        this.setData({
-          urls: this.data.cells
-        });
-      }
-      this.setData({
-        newOrOld
-      });
-    },
+    // 根据 size 不同，计算的图片显示大小不同
+    itemSizePercentage: null,
+    // 存放图片 url 的数组
+    value: null
   },
 
   observers: {
-    // fix #1075 urls属性更新以后，图片不显示
-    // solution urls 更新时重新判断
-    urls() {
-      if (this.data.cells === null) {
-        let newOrOld = this.judgeNewOrOld();
+
+    /**
+     * 监听 clear
+     * @param clear
+     */
+    clear(clear) {
+      if (clear) {
+        console.warn('clear 属性已废弃，请使用 linClearImage 函数' +
+          ' 或 urls 属性清空图片\n 详情信息参考：');
         this.setData({
-          newOrOld
+          value: [],
+          clear: false
         });
       }
-    }
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-    handleClear() {
-      let urls = this.data.urls;
-      this.setData({
-        urls: [],
-        clear: false,
-        showBtn: true
-      });
-      let info = {
-        all: urls,
-        current: urls,
-      };
-
-      let option = {};
-      this.triggerEvent('linclear', info, option);
     },
 
-    // 预览 preview
-    onPreviewTap(e) {
-      const index = e.currentTarget.dataset.index;
-      const urls = this.data.urls;
-      let tempFilePath = '';
-      let previewImageList = [];
-      const newOrOld = this.data.newOrOld;
-      const cellsIsObject = Object.prototype.toString.call(this.data.cells) === '[object Object]';
-
-      // 第一个 if 是对 cells 的兼容处理
-      if (cellsIsObject) {
-        const cells = this.data.cells;
-        tempFilePath = cells[index].url;
-        for (let i = 0; i < cells.length; i++) {
-          previewImageList.push(cells[i].url);
-        }
-      } else if (newOrOld === 'old') {
-        tempFilePath = this.data.urls[index];
-        previewImageList = this.data.urls;
-
-      } else {
-        tempFilePath = this.data.urls[index].url;
-        for (let i = 0; i < urls.length; i++) {
-          previewImageList.push(urls[i].url);
-        }
-      }
-
-
-      let detail = {
-        index, // 下标
-        current: urls[index], // 当前显示图片的http链接
-        all: urls // 需要预览的图片http链接列表
-      };
-      let option = {};
-      if (this.data.preview === true) {
-        wx.previewImage({
-          current: tempFilePath, // 当前显示图片的http链接
-          urls: previewImageList // 需要预览的图片http链接列表
-        });
-      }
-      this.triggerEvent('linpreview', detail, option);
-    },
-
-
-    // 增加 add
-    onAddTap() {
-      const that = this;
-      const count = this.data.count - this.data.urls.length;
-      if (count === 0) {
+    /**
+     * 监听 urls、cells 更新 value 字段
+     * @param urls
+     * @param cells
+     */
+    'urls,cells': function (urls, cells) {
+      if (!urls && !cells) {
         return;
       }
-      const newOrOld = this.data.newOrOld;
-      wx.chooseImage({
-        count,
-        sizeType: this.data.sizeType,
-        sourceType: ['album', 'camera'],
-        success(res) {
-          // tempFilePath可以作为img标签的src属性显示图片
-          let tempFilePath = [];
-          if (newOrOld === 'old') {
-            tempFilePath = res.tempFilePaths;
-          } else {
-            for (let i = 0; i < res.tempFilePaths.length; i++) {
-              tempFilePath.push({
-                url: res.tempFilePaths[i],
-                // key: null
-                imageSize: res.tempFiles[i].size
-              });
-              tempFilePath[i].overSize = res.tempFiles[i].size > that.data.maxImageSize;
-            }
-          }
-          const newtempFilePaths = that.data.urls.concat(tempFilePath);
-          // 判断是否还能继续添加图片
-          if (newtempFilePaths.length === parseInt(that.data.count)) {
-            that.setData({
-              showBtn: false
-            });
-          }
-          that.setData({
-            urls: newtempFilePaths,
-            value: newtempFilePaths,
-            tempFilePath
-          });
-          let detail = {
-            current: tempFilePath,
-            all: newtempFilePaths
-          };
-          let option = {};
 
-          that.triggerEvent('linchange', detail, option);
-          that.triggerEvent('linpush', detail, option);
-
-          // 超过大小的image集合
-          let overSizeList = [];
-          for (let n = 0; n < newtempFilePaths.length; n++) {
-            if (newtempFilePaths[n].overSize) {
-              overSizeList.push(newtempFilePaths[n]);
-            }
-          }
-
-          if (overSizeList.length > 0) {
-            let detail = {
-              current: tempFilePath,
-              all: newtempFilePaths,
-              overSizeList: overSizeList,
-            };
-            that.triggerEvent('linoversize', detail, option);
+      let value = [];
+      if (urls) {
+        value = urls;
+      } else if (cells) {
+        for (const cell of cells) {
+          if (Object.prototype.hasOwnProperty.call(cell, 'url')) {
+            value.push(cell.url);
           }
         }
-      });
-
-    },
-
-    // 删除 remove
-    onDelTap(e) {
-      const index = e.currentTarget.dataset.index;
-      const urls = this.data.urls;
-      const tempFilePath = urls[index];
-      const tempFilePaths = this.handleSplice(urls, tempFilePath);
-      // 判断是否还能继续添加图片
-      if (tempFilePaths.length < parseInt(this.data.count)) {
-        this.setData({
-          showBtn: true
-        });
       }
+
       this.setData({
-        tempFilePath,
-        urls: tempFilePaths,
-        value: tempFilePaths,
+        value: value.slice(0, this.data.count)
       });
-      let detail = {
-        index,
-        current: tempFilePath,
-        all: tempFilePaths
-      };
-      let option = {};
-
-      this.triggerEvent('linremove', detail, option);
-
-    },
-    handleSplice(arr, current) {
-      return arr.filter(item => item !== current);
     },
 
-    judgeNewOrOld: function () {
-      const urls = this.data.urls;
-      if (urls.length !== 0) {
-        if (typeof (urls[0]) !== 'object') {
-          return 'old';
-        }
-        return 'new';
+    /**
+     * size 属性变化时，重新调整图片大小
+     * @param size 新值
+     */
+    async size(size) {
+      if (!size) {
+        this.setData({itemSizePercentage: null});
+        return;
       }
-      return 'new';
-    }
 
+      // 获取 .lin-image-picker__container 容器宽度
+      const res = await nodeUtil.getNodeRectFromComponent(this, '.lin-image-picker__container');
+      const widthRpx = deviceUtil.px2rpx(res.right - res.left);
+
+      // 根据容器宽度计算单张图片宽度百分比
+      const itemSizePercentage = ((10 / size * 10) - 20 / widthRpx * 100) + '%;';
+      this.setData({itemSizePercentage});
+    },
+
+    custom(custom) {
+      if (custom) {
+        console.warn('custom 已弃用，请勿使用该属性，直接传入 slot 即可');
+      }
+    }
   },
+
+  methods: {
+
+    // ============== 事件监听函数 =============
+
+    /**
+     * 点击图片触发的事件
+     * 进行图片预览，抛出自定义事件
+     * @param e
+     */
+    onTapImage(e) {
+      const that = this;
+      const value = this.data.value;
+      const imageIndex = e.currentTarget.dataset.imageIndex;
+      const imageUrl = value[imageIndex];
+      const detail = {
+        all: value,
+        index: imageIndex,
+        current: imageUrl
+      };
+
+      eventUtil.emit(this, 'lintap', detail);
+
+      // 预览图片
+      if (this.data.preview) {
+        wx.previewImage({
+          urls: value,
+          current: imageUrl,
+          success() {
+            eventUtil.emit(that, 'linpreview', detail);
+          }
+        }, true);
+      }
+    },
+
+    /**
+     * 点击删除按钮触发的事件
+     * @param e 事件对象
+     */
+    onTapRemove(e) {
+      const value = this.data.value;
+      const imageIndex = e.currentTarget.dataset.imageIndex;
+      const imageUrl = value[imageIndex];
+      const detail = {
+        all: value,
+        index: imageIndex,
+        current: imageUrl
+      };
+
+      eventUtil.emit(this, 'linremovetap', detail);
+
+      if (this.data.remove) {
+        this._removeImageByIndex(imageIndex);
+      }
+    },
+
+    /**
+     * 点击添加按钮触发的事件
+     * @returns {Promise<void>}
+     */
+    async onTapAdd() {
+      let {value, count, sizeType} = this.data;
+      const remainCount = count - value.length;
+      if (value.length >= count || remainCount <= 0) {
+        return;
+      }
+
+      // 调用微信 api 选择图片
+      const chooseImageRes = await promisic(wx.chooseImage)({
+        count: remainCount,
+        sizeType,
+        sourceType: ['album', 'camera'],
+      });
+
+      // 即将被添加的图片的 url 数组
+      const addImageUrlArray = [];
+      // 超过了大小限制的图片的 url 数组，不会被添加到 image-picker 中
+      const oversizeImageUrlArray = [];
+
+      chooseImageRes.tempFiles.forEach((tempFile) => {
+        const {path, size} = tempFile;
+        if (size > this.data.maxImageSize) {
+          oversizeImageUrlArray.push(path);
+        } else {
+          addImageUrlArray.push(path);
+        }
+      });
+
+      // 赋值并触发事件
+      this.setData({
+        value: value.concat(addImageUrlArray)
+      }, () => {
+        const detail = {
+          all: this.data.value,
+          current: addImageUrlArray,
+          oversize: oversizeImageUrlArray
+        };
+        eventUtil.emit(this, 'linadd', detail);
+        // todo 1.0.0 版本去除 linchange 事件
+        eventUtil.emit(this, 'linchange', detail);
+        // todo 1.0.0 版本去除 linoversize 事件
+        eventUtil.emit(this, 'linoversize', detail);
+      });
+    },
+
+    /**
+     * 供 Form 组件调用的取值方法
+     * @returns {*}
+     */
+    getValue() {
+      return this.data.value;
+    },
+
+    // ======= 内部函数 =========
+    /**
+     * 根据图片索引删除图片
+     * @param index 图片索引
+     * @private
+     */
+    _removeImageByIndex(index) {
+      const value = this.data.value;
+      const current = value[index];
+      value.splice(index, 1);
+
+      const detail = {
+        index,
+        current,
+        all: value
+      };
+      this.setData({value}, () => {
+        eventUtil.emit(this, 'linremove', detail);
+      });
+    },
+
+    // ============== 开放函数 ==============
+    /**
+     * 删除图片
+     * @param imageIndex 图片索引
+     */
+    linRemoveImage(imageIndex) {
+      this._removeImageByIndex(imageIndex);
+    },
+
+    /**
+     * 清空所有图片
+     * 不会触发 linremove 事件
+     */
+    linClearImage() {
+      this.setData({
+        value: []
+      });
+    },
+
+    /**
+     * 获取所有 url 图片链接
+     * @returns {Array<String>}
+     */
+    linGetValue() {
+      return this.data.value;
+    }
+  }
 });
